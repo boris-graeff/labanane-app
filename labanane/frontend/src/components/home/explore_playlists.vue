3b52bf5cd469f40b4cefe9615ace44b70b50cf8c
Édouard Lopez <dev+git@edouard-lopez.com>
2016-12-08 00:22:14 +0100

    reformat with tabs as recommended by xo

--- labanane/frontend/src/components/home/explore_playlists.vue
+++ labanane/frontend/src/components/home/explore_playlists.vue
@@ -1,40 +1,40 @@
 <template>
-  <div>
-    <div class='text-box'>
-      <input type='text' id='playlist-filter' />
-      <label for='playlist-filter'>Playlist name</label>
+    <div>
+        <div class='text-box'>
+            <input type='text' id='playlist-filter'/>
+            <label for='playlist-filter'>Playlist name</label>
+        </div>
+        <ul class='playlists'>
+            <li v-for='p in playlists'>
+                <router-link :to="{ name: 'playlist', params: { id: p}}">{{p}}</router-link>
+            </li>
+        </ul>
     </div>
-    <ul class='playlists'>
-      <li v-for='p in playlists'>
-        <router-link :to="{ name: 'playlist', params: { id: p}}">{{p}}</router-link>
-      </li>
-    </ul>
-  </div>
 </template>
 
 <script>
-  import actions from '../../actions'
+	import actions from '../../actions'
 
-  export default {
-    name: 'explore-playlists',
-    mounted () {
-      this.get_playlists();
-    },
-    vuex: {
-      getters: {
-        playlists: state => state.playlists
-      },
-      actions: {
-        get_playlists: actions.get_playlists
-      }
-    }
-  }
+	export default {
+		name: 'explore-playlists',
+		mounted () {
+			this.get_playlists();
+		},
+		vuex: {
+			getters: {
+				playlists: state => state.playlists
+			},
+			actions: {
+				get_playlists: actions.get_playlists
+			}
+		}
+	}
 </script>
 
 <style lang='sass'>
     @import 'src/styles/constants.scss';
 
-  .playlists {
-    margin-top: $space-medium;
-  }
+    .playlists {
+        margin-top: $space-medium;
+    }
 </style>
