ebad1ed1296ec26e4cd149c5c76960017a0fa07d
Boris Graeff <boris.graeff@gmail.com>
2016-12-09 14:21:18 +0100

    Sets up playlist creation form

--- labanane/frontend/src/components/home/create_playlist.vue
+++ labanane/frontend/src/components/home/create_playlist.vue
@@ -3,14 +3,14 @@
     <div class='tips'>
       Tips: please note that you will not be able to reset your password
     </div>
-    <form>
+    <form @submit='create'>
       <div class='text-box'>
         <label for='playlist-name'>Name</label>
-        <input type='text' id='playlist-name'>
+        <input type='text' id='playlist-name' v-model='name'>
       </div>
       <div class='text-box'>
         <label for='playlist-password'>Password</label>
-        <input type='password' id='playlist-password'>
+        <input type='password' id='playlist-password' v-model='password'>
       </div>
       <button type='submit'>Create</button>
     </form>
@@ -18,11 +18,33 @@
 </template>
 
 <script>
+  import actions from '../../actions'
 
   export default {
     name: 'create-playlist',
+    data: function () {
+      return {
+        name: '',
+        password: ''
+      }
+    },
+    methods : {
+      create(event) {
+        event.preventDefault()
 
+        if(this.name && this.password){
+
+          this.createPlaylist(this.name, this.password)
+        }
+      }
+    },
+    vuex: {
+      actions: {
+        createPlaylist: actions.createPlaylist
+      }
+    }
   }
+
 </script>
 
 <style lang='sass'>
