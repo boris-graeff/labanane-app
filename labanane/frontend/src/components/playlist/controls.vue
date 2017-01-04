dd6328d83fd08131c9d8fca40de795055eeceb6a
Boris Graeff <boris.graeff@gmail.com>
2017-01-04 14:29:04 +0100

    Adds icons on player buttons

--- labanane/frontend/src/components/playlist/controls.vue
+++ labanane/frontend/src/components/playlist/controls.vue
@@ -1,24 +1,23 @@
 <template>
   <div class='controls'>
-    {{ player.state }}
     <div class='progress bar'>
       <input type='range' v-model='progression' />
     </div>
     <ul>
       <li>
-        <button type='button' @click='prev'>Previous</button>
+        <button type='button' class='btn-previous' @click='prev'></button>
       </li>
       <li v-show='player.state !== "playing"'>
-        <button type='button' @click='play'>Play</button>
+        <button type='button' class='btn-play' @click='play'></button>
       </li>
       <li v-show='player.state === "playing"'>
-        <button type='button' @click='pause'>Pause</button>
+        <button type='button' class='btn-pause' @click='pause'></button>
       </li>
       <li>
-        <button type='button' @click='next'>Next</button>
+        <button type='button' class='btn-next' @click='next'></button>
       </li>
       <li>
-        <button type='button'>Random</button>
+        <button type='button' class='btn-shuffle'></button>
       </li>
     </ul>
   </div>
@@ -63,7 +62,39 @@
     ul {
       display: flex;
       justify-content: space-around;
+
+      li {
+        width: 20%;
+      }
     }
 
+    button {
+      width: 100%;
+      height: 60px;
+      background-color: transparent;
+      background-repeat: no-repeat;
+      background-size: contain;
+      background-position: center center;
+    }
+  }
+
+  .btn-next {
+    background-image: url('/images/icn-next.svg');
+  }
+
+  .btn-previous {
+    background-image: url('/images/icn-previous.svg');
+  }
+
+  .btn-play {
+    background-image: url('/images/icn-play.svg');
+  }
+
+  .btn-pause {
+    background-image: url('/images/icn-pause.svg');
+  }
+
+  .btn-shuffle {
+    background-image: url('/images/icn-shuffle.svg');
   }
 </style>
