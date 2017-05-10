5a892d6da401f805eb1b3794897362205fb82ebd
Boris Graeff <boris.graeff@gmail.com>
2017-05-10 17:50:04 +0200

    Fixes frontend path on prod build

--- backend/app.js
+++ backend/app.js
@@ -21,12 +21,12 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, '../frontend/dist/')))
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')))
 
 app.use('/services/', services)
 
 app.get('*', function(req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'frontend', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '..', 'frontend', 'dist', 'index.html'))
 })
 
 // catch 404 and forward to error handler
