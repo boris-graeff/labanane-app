7bcf851d1c22693d5b38e811eb650659e0ca7ecc
Boris <boris.graeff@gmail.com>
2017-12-20 18:25:11 +0100

    Add temporary style for christmas

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class='app'>
+  <div class='app' id='snow'>
     <transition name='page' mode='out-in'>
       <router-view></router-view>
     </transition>
@@ -8,12 +8,90 @@
 
 <script>
   export default {
-    name: 'app'
+    name: 'app',
+    mounted () {
+      window.particlesJS('snow', {
+        'particles': {
+          'number': {
+            'value': 100,
+            'density': {
+              'enable': true,
+              'value_area': 800
+            }
+          },
+          'color': {
+            'value': '#ffffff'
+          },
+          'shape': {
+            'type': 'circle',
+            'stroke': {
+              'width': 0,
+              'color': '#000000'
+            },
+            'image': {
+              'src': 'img/github.svg',
+              'width': 100,
+              'height': 100
+            }
+          },
+          'opacity': {
+            'value': 1,
+            'random': true,
+            'anim': {
+              'enable': true,
+              'speed': 1,
+              'opacity_min': 0.1,
+              'sync': false
+            }
+          },
+          'size': {
+            'value': 5,
+            'random': true,
+            'anim': {
+              'enable': true,
+              'speed': 2,
+              'size_min': 0.1,
+              'sync': false
+            }
+          },
+          'line_linked': {
+            'enable': false
+          },
+          'move': {
+            'enable': true,
+            'speed': 2,
+            'direction': 'bottom',
+            'random': false,
+            'straight': false,
+            'out_mode': 'out',
+            'bounce': false,
+            'attract': {
+              'enable': false,
+              'rotateX': 600,
+              'rotateY': 1200
+            }
+          }
+        },
+        'interactivity': {
+          'detect_on': 'canvas',
+          'events': {
+            'onhover': {
+              'enable': false
+            },
+            'onclick': {
+              'enable': false
+            },
+            'resize': false
+          }
+        },
+        'retina_detect': true
+      })
+    }
   }
 </script>
 
 <style lang='scss' rel='stylesheet/scss' type='text/css'>
-  @import "~@/styles/base.scss";
+  @import '~@/styles/base.scss';
 </style>
 
 <style scoped lang='scss' rel='stylesheet/scss' type='text/css'>
@@ -24,5 +102,17 @@
 
   .page-enter, .page-leave-to {
     opacity: 0;
+  }
+</style>
+
+<style>
+  #snow > canvas {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh !important;
+    width: 100vw !important;
+    pointer-events: none;
+    z-index: 1;
   }
 </style>
